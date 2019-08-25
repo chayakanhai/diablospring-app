417a23084e4ff4484bd13f216c211332862534a5
Chaya Kanhaigit <chayakanhai@msn.com>
2019-08-25 23:20:36 +0200

    demo

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -11,14 +11,16 @@ class Dashboard extends Component {
     //console.log(this.props);
     const { demos, auth } = this.props;
     if (!auth.uid) return <Redirect to="/signin" />;
+
     return (
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
             <DemoList demos={demos} />
           </div>
-          <div className="col s12 m5 offset-m1" />
-          <Notifications />
+          <div className="col s12 m5 offset-m1">
+            <Notifications />
+          </div>
         </div>
       </div>
     );
