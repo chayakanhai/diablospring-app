fcb0e77bd3a0096ac198e3b888d9218c997ba49f
Chaya Kanhaigit <chayakanhai@msn.com>
2019-08-26 16:01:10 +0200

    error hosting to server takes firebase index.html

---
+++ dist/build/precache-manifest.baad98d760a482be964bbb1419c3e24a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9c82b23f57dc05ea629323625deb84f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27b3d17541d60f4bae76",
+    "url": "/static/css/main.db37b7d2.chunk.css"
+  },
+  {
+    "revision": "f76f63220a07e2ee7390",
+    "url": "/static/js/2.ce48b9dc.chunk.js"
+  },
+  {
+    "revision": "27b3d17541d60f4bae76",
+    "url": "/static/js/main.cca6416a.chunk.js"
+  },
+  {
+    "revision": "8e4a2cf4c119fe111a23",
+    "url": "/static/js/runtime~main.03633908.js"
+  },
+  {
+    "revision": "f2ffaf45dc2d3de434e77d43e6586450",
+    "url": "/static/media/background.f2ffaf45.jpg"
+  }
+]);
